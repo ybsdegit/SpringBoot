86bfd1652faa70615c71bb909504232daa845646
ybsdegit <ybsdeyx@163.com>
2020-05-04 12:11:18 +0800

    alisms

---
+++ alisms/src/main/java/com/ybs/alisms/AlismsApplication.java
@@ -0,0 +1,13 @@
+package com.ybs.alisms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlismsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlismsApplication.class, args);
+    }
+
+}
---
+++ alisms/src/test/java/com/ybs/alisms/AlismsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ybs.alisms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlismsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
