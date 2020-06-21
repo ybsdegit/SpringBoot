dd98cbfc229ff39f95289763a4bd2b85d2043903
ybsdegit <ybsdeyx@163.com>
2020-06-22 01:37:10 +0800

    定时任务

--- spring-sync/src/main/java/com/ybs/service/ScheduleService.java
+++ spring-sync/src/main/java/com/ybs/service/ScheduleService.java
@@ -17,8 +17,11 @@ public class ScheduleService {
     // cron 表达式
     // 秒 分 时 日 月 周几
     // 30 0/5 10,18 * * ? 每天10点和18点，每5分钟执行一次
+    // , 枚举
+    // - 区间
+    // / 步长
     @Scheduled(cron = "0/5 * * * * 0-7")
-    public void hello(){
+    public void hello() {
         System.out.println("hello, 你被执行了~");
     }
 }
